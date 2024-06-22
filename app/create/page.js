c1b32f76cda1a37605ebdb9d265051033b83117b
Flames <isaacomenuche@gmail.com>
2024-06-22 04:20:01 +0100

    adjust roadmap

--- app/create/page.js
+++ app/create/page.js
@@ -1,10 +1,17 @@
 import CreateGovernance from "@/components/CreateGovernance";
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
   return (
     <div>
       <CreateGovernance />
+      <Link
+        href={"/demo"}
+        className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Create
+      </Link>
     </div>
   );
 }
--- components/Feature.js
+++ components/Feature.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Feature({ progress, children }) {
   return (
     <li
-      className={`bg-orange-500 text-white text-lg text-center p-4 ${
+      className={`bg-orange-500 hover:bg-orange-600 text-white text-lg text-center p-4 ${
         progress && "bg-[url(/check.svg)]"
       }  bg-no-repeat bg-center bg-cover rounded-lg w-full max-w-[300px] h-full`}
     >
