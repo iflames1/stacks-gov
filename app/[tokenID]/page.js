a27eb4c5d16ae8bb9c8baf420b74608dce0b925f
Flames <isaacomenuche@gmail.com>
2024-06-22 03:14:43 +0100

    remove Topic.js

--- app/[tokenID]/page.js
+++ app/[tokenID]/page.js
@@ -1,13 +1,11 @@
 import GovernancePage from "@/components/proposals/GovernancePage";
 import Add from "@/components/proposals/Add";
-import Topics from "@/components/proposals/Topics";
 import React from "react";
 
 export default function page({ params }) {
   return (
     <div className="min-h-screen mb-20">
       <h1>Welcome to {params.tokenID} governance page</h1>
-      <Topics />
       <GovernancePage />
       <Add path={"/propose"} />
     </div>
