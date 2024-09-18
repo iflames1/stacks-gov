c45c7aed960b7c301480005c16302cb4c4d01642
Flames <isaacomenuche@gmail.com>
2024-09-18 22:34:08 +0100

    fix: navigation width alignment

--- src/components/home/home-nav.tsx
+++ src/components/home/home-nav.tsx
@@ -3,13 +3,13 @@ import Nav from "./nav";
 export default function HomeNav({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
-      <div className="w-[12vw] h-[calc(100vh-106.59px)] border-r border-white/15 flex flex-col gap-4 pt-6">
+      <div className="w-[13.7vw] h-[calc(100vh-106.59px)] border-r border-white/15 flex flex-col gap-4 pt-6">
         <Nav title="Dashboard" path="/" />
         <Nav title="Explore" path="/explore" />
         <Nav title="Delegates" path="/delegates" />
         <Nav title="Activity" path="/activity" />
       </div>
-      {children}
+      <div className="w-full">{children}</div>
     </div>
   );
 }
