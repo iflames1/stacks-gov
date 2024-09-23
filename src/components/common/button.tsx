76c5ab68846f247ff563f275f3bb74fe56e3d466
Flames <isaacomenuche@gmail.com>
2024-09-23 07:55:11 +0100

    feat: add asChild prop to button

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,14 +1,23 @@
 import { cn } from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
 
 interface ButtonProps {
   className?: string;
   children?: React.ReactNode;
   onClick?: () => void;
+  asChild?: boolean;
 }
 
-export default function Button({ className, children, onClick }: ButtonProps) {
+export default function Button({
+  className,
+  children,
+  onClick,
+  asChild = false,
+}: ButtonProps) {
+  const Component = asChild ? Slot : "button";
+
   return (
-    <button
+    <Component
       onClick={onClick}
       className={cn(
         "flex items-center justify-center gap-3 border border-white/[0.02] rounded-[2px] bg-white/5 text-base font-normal cursor-pointer",
@@ -19,6 +28,6 @@ export default function Button({ className, children, onClick }: ButtonProps) {
       )}
     >
       {children}
-    </button>
+    </Component>
   );
 }
