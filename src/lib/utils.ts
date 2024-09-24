05ac953ad4ce4fd8b45ea64df21b248f765bdf95
Flames <isaacomenuche@gmail.com>
2024-09-24 18:44:56 +0100

    feat: add fetchProposal function to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { DAO } from "@/types/dao";
+import { DAO, PROPOSAL } from "@/types/dao";
 import axios from "axios";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
@@ -7,18 +7,43 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export async function fetchDao(username: string) {
-  console.log("hiii");
+export async function fetchDao(username: string): Promise<DAO | null> {
   try {
     const response = await axios.get("/data.json");
     const data: DAO[] = response.data;
-    const dao = data.find((dao) => dao.username.toLowerCase() === username);
+    const dao = data.find(
+      (dao) => dao.username.toLowerCase() === username.toLowerCase()
+    );
     if (!dao) {
       return null;
     }
     return dao;
   } catch (error) {
     console.error("Error fetching DAOs:", error);
+    return null;
+  }
+}
+
+export async function fetchProposal(
+  username: string,
+  proposalId: number
+): Promise<PROPOSAL | null> {
+  try {
+    const response = await axios.get("/data.json");
+    const data: DAO[] = response.data;
+    const dao = data.find(
+      (dao) => dao.username.toLowerCase() === username.toLowerCase()
+    );
+    if (!dao) {
+      return null;
+    }
+    const proposal = dao.proposals[proposalId];
+    if (!proposal) {
+      return null;
+    }
+    return proposal;
+  } catch (error) {
+    console.error("Error fetching proposal:", error);
     return null;
   }
 }
